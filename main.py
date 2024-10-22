c087904a5369572afb5cc7c971f14e490b3df9c5
FaiziXd <faiizuxd@gmail.com>
2024-10-22 23:21:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string, redirect, request
+import os
 
 app = Flask(__name__)
 
@@ -102,4 +103,6 @@ def visit():
     return redirect('https://steven-zqlg.onrender.com/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    
